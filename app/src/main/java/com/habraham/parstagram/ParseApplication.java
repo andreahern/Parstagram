f6f8787e899749f88890b47d33fb1c4dc2b71ae9
Abraham Hernandez <habraham@fb.com>
2020-07-06 16:08:47 -0400

    Project initialized & added login functionality

---
+++ app/src/main/java/com/habraham/parstagram/ParseApplication.java
@@ -0,0 +1,20 @@
+package com.habraham.parstagram;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+public class ParseApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        ParseObject.registerSubclass(Post.class);
+
+        Parse.initialize(new Parse.Configuration.Builder(this)
+        .applicationId("abe-parstagram")
+        .clientKey("LearnSomeParseMMM")
+        .server("https://abe-parstagram.herokuapp.com/parse/").build());
+    }
+}
